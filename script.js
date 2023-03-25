85d18d4d4dfebe17e0ccc04ffb0400c49bad59c6
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-25 14:58:14 +0700

    add code example to show that the last prnt statement could have been printed before the code block above it was done executing

--- script.js
+++ script.js
@@ -20,3 +20,33 @@ if (i === number - 1) {
 console.log(`We finished calculating the first
 ${array.length} prime numbers`);
 console.log("We had to wait for this line to print");
+
+
+console.log("We're calculating the first 12000 prime numbers");
+console.log("Waiting...");
+const array1 = [2];
+let number1 = 3;
+while (array1.length < 12000) {
+    for (let i = 2; i < number; i++) {
+if (number % i=== 0) {
+number++;
+break;
+}
+if (i === number1 - 1) {
+    array1.push(number);
+    number++;
+    break;
+}
+    }
+}
+console.log(`We finished calculating the first ${array1.length} prime numbers`);
+console.log("We had to wait even longer for this line to print");
+
+
+
+
+
+
+
+
+
