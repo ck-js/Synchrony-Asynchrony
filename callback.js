c244c54c73d69479172f2b254c6889eb0419bf98
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-27 15:03:44 +0700

    Create a function that had one template literal and one call back function as parameters, then in function print the string literal then the call back function. Create callback function then add print statement then call the first function with string value and call back function name

--- callback.js
+++ callback.js
@@ -3,7 +3,7 @@
 function callbackFunc() {
     console.log("Executed last because we're using asynchronous code");
     };
-    setTimeout(callbackFunc, 1500);
+    setTimeout(callbackFunc, 1000);
     console.log("Executed first");
     console.log("Executed second");
     
@@ -14,6 +14,18 @@ function callbackFunc() {
         console.log("Executed last because we're using asynchronous code");
      }
     setTimeout(reverseCallback, 1500);
-    
-    
-    
+console.log("Executed first, before the word was reversed");
+    // notice how the first call back prints after the first console log of second call back //
+
+// deeply nested call backs are beneficial but can get confusing and undersirable //
+
+const sayHello = (name, callback) => {
+    console.log(`Hey there, ${name}`);
+ callback();   
+}
+const sayGoodbye = () => {
+    console.log("Goodbye");
+}
+sayHello("Vladimir", sayGoodbye);
+
+
