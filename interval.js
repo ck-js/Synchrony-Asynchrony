b0740039f1a7b0849ebf81a25e545c2830d5c4dc
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-27 21:18:31 +0700

    Add new file for the interval method but the code example runs forever and did not include the clear interval method into our code block which is the next code example that will be implemented.

---
+++ interval.js
@@ -0,0 +1,11 @@
+
+let blinkerOn = false;
+const toggleBlinker = () => {
+    if (blinkerOn) {
+        console.log("Blinker Off");
+        blinkerOn = false;
+    }else {
+        console.log("Blinker On");
+        blinkerOn = true;
+    }
+};
