61c0bde783c42c51814b0b5b10262156058877e8
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-28 11:23:28 +0700

    Add blinker interval function with the set interval, set time out and clear interval methods so that the function would run at intervals of 500ms, display a message at each side of the interval and then stop at 25000ms and display a stop message

--- interval.js
+++ interval.js
@@ -1,11 +1,18 @@
+// delete first code example as it ran forever //
+// noww writing revised version that won't run forever //
 
 let blinkerOn = false;
-const toggleBlinker = () => {
-    if (blinkerOn) {
-        console.log("Blinker Off");
-        blinkerOn = false;
-    }else {
-        console.log("Blinker On");
-        blinkerOn = true;
-    }
-};
+let blinkerInterval = setInterval(() => {
+if (blinkerOn) {
+    console.log("Blinker Off");
+    blinkerOn = false;
+} else {
+console.log("Blinker On");
+blinkerOn = true;    
+}
+}, 500);
+setTimeout(() => {
+    clearInterval(blinkerInterval);
+    console.log("Stop");
+}, 2500);
+
