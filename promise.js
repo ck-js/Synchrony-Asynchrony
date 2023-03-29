1ce7f3f828479ce8f988c9086172029944937a6a
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-29 11:18:40 +0700

    Add promise with inline function and pass resolve and reject as parameteres then wrote print statements for both resolve and reject methods depending on the condition of the if statement that checks the stock of our dessert object.

--- promise.js
+++ promise.js
@@ -6,22 +6,33 @@ const aBrandNewPromise = new Promise(function() {
 });
 
 const dessertStock = {
-    cheeseCake: 5,
+    cheeseCake: 0,
     croissant: 20,
-    custard: 0,
+    custard: 1,
 } 
 function orderCheesecake() {
 return new Promise(function(resolve, reject) {
 if (dessertStock.cheeseCake > 0) {
     resolve("Cheesecake is available, your order has been processed");
+} else {
+    reject("Cheese is not available therefore cheesecake is out of stock");
 }
 });    
 }
-const makeOrder = orderCheesecake();
-console.log(makeOrder);
+const makeOrder1 = orderCheesecake();
+console.log(makeOrder1);
 
-
-
+function orderCustard() {
+    return new Promise(function(resolve, reject) {
+    if (dessertStock.custard > 0) {
+        resolve("Custard order successful");
+    } else {
+        reject("Custard is not available right now");
+    }
+    });
+}
+const makeOrder2 = orderCustard();
+console.log(makeOrder2);
 
 
 
