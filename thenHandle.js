db049ed237ac7b4168aa191602f9ea550816476c
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-30 20:44:10 +0700

    Add an object with a parent element and 2 child elements then two functions with new promise methods. First function to determine if the object stock is available then the resolve and reject methods gave parameters of true and false. Then the second parameter checked the availibility by passing the first function results into the parameter of the second function which we will do in the handle is available method. Then create two handle functions that return the result of the first function and the second one displayed the output message of the second if else statement.

--- thenHandle.js
+++ thenHandle.js
@@ -15,8 +15,50 @@ return new Promise(function(resolve, reject) {
 function handleSuccess(successValue) {
     console.log(successValue);
 };
+function handleFailure(failureValue) {
+    console.log(failureValue);
+}
+
 const makeOrder1 = orderCheesecake();
-makeOrder1.then(handleSuccess);
+makeOrder1.then(handleSuccess).catch(handleFailure);
+
+const flowerStock = {
+    girlScoutCookies: {
+        inStockAmount: 10,
+        price: 450,
+    },
+    whiteWidow: {
+        inStockAmount: 6,
+        price: 350,
+    },
+    };
+    const flowerBudget = 600;
+function isAvailable() {
+    return new Promise(function(resolve, reject) {
+if (flowerStock.girlScoutCookies.inStockAmount > 0) {
+resolve(true);
+        }else {
+            reject(false);
+        }
+    });
+}
+function checkPrice(productAvailable) {
+    return new Promise(function(resolve, reject) {
+        if (productAvailable && flowerStock.girlScoutCookies.price <= flowerBudget) {
+        resolve("Good price and within my budget");
+        }else {
+            reject("Too expensive and out of my budget");
+        }
+    });
+}
+function handleIsAvailable(isAvailable) {
+    return checkPrice(isAvailable);
+}
+function handleCheckPrice(successValue) {
+    console.log(successValue);
+}
+const checkAvailability = isAvailable();
+checkAvailability.then(handleIsAvailable).then(handleCheckPrice);
 
 
 
