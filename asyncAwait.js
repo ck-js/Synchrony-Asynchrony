f258c0d86a6aad4ebb1a461185bcf32207d50a91
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-02 18:07:22 +0700

    Add new project file for async and await code examples. Wrote an async function with the async keyword before the function keyword. Create new promise and store in a function then use the set time out function to display after 2 seconds which also triggered the resolve promise

---
+++ asyncAwait.js
@@ -0,0 +1,35 @@
+
+
+async function asyncFunc() {
+    console.log("I am an async function.");
+}
+asyncFunc();
+
+let traditionalPromise = new Promise((resolve, reject) => {
+setTimeout(() => resolve(
+"This promise will always resolve after 2 seconds"),
+ 2000);
+});
+async function asyncFunc1() {
+let value = await traditionalPromise;
+console.log(value);
+}
+asyncFunc1();
+
+let beverages = ["Tea", "Coffee", "Apple Cider"];
+
+function pickRandomBeverage() {
+return new Promise(function(resolve, Reject) {
+let randomIndex = Math.floor(Math.random() *
+beverages.length);
+let selectedBeverage = beverages[randomIndex];
+setTimeout(function() {
+console.log(`${selectedBeverage} selected`);
+resolve(selectedBeverage);
+}, 1000);
+});
+}
+
+
+
+
